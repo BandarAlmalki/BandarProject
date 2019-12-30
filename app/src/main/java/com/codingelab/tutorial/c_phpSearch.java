2a1d325c34026603753e22bea836dcb44d04a090
Star <202392406@student.ttcollege.edu.sa>
2019-12-30 15:30:48 +0300

    Initial commit

---
+++ app/src/main/java/com/codingelab/tutorial/c_phpSearch.java
@@ -0,0 +1,13 @@
+package com.codingelab.tutorial;
+
+public class c_phpSearch {
+        public static final String DATA_URL = "http://192.168.43.58:8080/sqli/fetch.php?email=";
+        public static final String ID = "id";
+        public static final String NAME = "name";
+        public static final String PHONE = "phone";
+        public static final String EMAIL = "email";
+        public static final String ARRAY = "result";
+        public int id;
+        public String email;
+
+    }
